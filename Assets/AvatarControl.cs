5a41e3449e7c467bbe1fd5fddf9edaea92bc5f96
ppesterev <ppesterev@mail.ru>
2019-10-09 21:38:08 +0700

    Add player and avatar prefabs, basic networking, synced movement

---
+++ Assets/AvatarControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AvatarControl : NetworkBehaviour
+{
+    [SerializeField]
+    float speed = 5f;
+    CharacterController controller = null;
+    Vector3 motion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hasAuthority)
+        {
+            motion.Set(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            motion.Normalize();
+            if (motion.magnitude > 0.05f)
+                transform.forward = Vector3.RotateTowards(transform.forward, motion, 0.3f, 1);
+            controller.Move(motion * speed * Time.fixedDeltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerPresence.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerPresence : NetworkBehaviour
+{
+    [SerializeField]
+    GameObject avatarPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // each player presence on the server spawns their selected avatar
+        if (isServer)
+        {
+            SpawnAvatar();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnAvatar()
+    {
+        GameObject avatar = Instantiate(avatarPrefab);
+        NetworkServer.SpawnWithClientAuthority(avatar, this.gameObject);
+    }
+}
