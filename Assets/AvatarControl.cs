bced0f063759461eb4c5dada7d12da05e23b9b57
ppesterev <ppesterev@mail.ru>
2019-10-09 23:11:01 +0700

    Run & attack animations, some broken networking

--- Assets/AvatarControl.cs
+++ Assets/AvatarControl.cs
@@ -8,24 +8,39 @@ public class AvatarControl : NetworkBehaviour
     [SerializeField]
     float speed = 5f;
     CharacterController controller = null;
+    Animator animator = null;
     Vector3 motion;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(hasAuthority)
         {
             motion.Set(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            motion.Normalize();
-            if (motion.magnitude > 0.05f)
+
+            if (motion.magnitude > 0.03f)
                 transform.forward = Vector3.RotateTowards(transform.forward, motion, 0.3f, 1);
             controller.Move(motion * speed * Time.fixedDeltaTime);
+
+            if(Input.GetButtonDown("Fire1"))
+            {
+                CmdQuickAttack();
+            }
         }
+
+        animator.SetFloat("Speed", motion.magnitude);
+    }
+
+    [Command]
+    void CmdQuickAttack()
+    {
+        Debug.Log("Command called");
+        animator.SetTrigger("QuickAttack");
     }
 }
