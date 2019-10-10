8df0b840a97d173f971247c15519593da4e91322
ppesterev <ppesterev@mail.ru>
2019-10-10 19:06:43 +0700

    Add materials from asset store, create terrain prefabs

---
+++ Assets/WaterAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterAnimation : MonoBehaviour
+{
+    MeshRenderer mr = null;
+    Vector2 uvOffset = Vector2.zero;
+    [SerializeField]
+    Vector2 motion = new Vector2(0.1f, 0.02f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uvOffset += motion * Time.deltaTime;
+        mr.material.SetTextureOffset("_MainTex", uvOffset);
+    }
+}
